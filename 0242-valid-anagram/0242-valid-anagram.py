db3284509b6bb369d97aa1cb465976b6e04f2117
Kiran-Raj-R <56603899+Kiran-Raj-R@users.noreply.github.com>
2025-08-20 09:32:32 +0530

    Time: 15 ms (42.3%), Space: 18 MB (50.12%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,5 +1,7 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
         freq1,freq2 = {},{}
         for ch in s:
             if ch in freq1:
@@ -13,7 +15,7 @@ class Solution:
             else:
                 freq2[c] = 1
         
-        if len(s) == len(t) and freq1 == freq2:
+        if freq1 == freq2:
             return True
         else:
             return False

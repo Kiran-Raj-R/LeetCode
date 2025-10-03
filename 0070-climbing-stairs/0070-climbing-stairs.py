5068b302220e6f478de4b3e1782826f1f8e4d557
Kiran-Raj-R <56603899+Kiran-Raj-R@users.noreply.github.com>
2025-10-03 09:36:12 +0530

    Time: 0 ms (100%), Space: 17.7 MB (48.87%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,17 +1,7 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        # s,m = 1,1
-        # for i in range(n-1):
-        #     s = s + m
-        #     m = s - m
-        # return s
-        
-        if n <= 2:
-            return n
-
-        prev, curr = 1, 1   # prev = ways(n-2), curr = ways(n-1)
+        current ,previous = 1,1
         for i in range(n-1):
-            temp = curr
-            curr = curr + prev   # ways(i) = ways(i-1) + ways(i-2)
-            prev = temp          # shift forward
-        return curr
+            current = current + previous
+            previous = current - previous
+        return current    

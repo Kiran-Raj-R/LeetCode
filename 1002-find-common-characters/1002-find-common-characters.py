0ec17d18f7692c7a892ee5a7909ea5b6a4c3248e
Kiran-Raj-R <56603899+Kiran-Raj-R@users.noreply.github.com>
2025-10-06 10:22:21 +0530

    Time: 7 ms (84.9%), Space: 17.8 MB (65.07%) - LeetHub

--- 1002-find-common-characters/1002-find-common-characters.py
+++ 1002-find-common-characters/1002-find-common-characters.py
@@ -1,6 +1,15 @@
 class Solution:
     def commonChars(self, words: List[str]) -> List[str]:
-        freq = Counter(words[0])
-        for word in words:
-            freq &= Counter(word)
-        return list(freq.elements())
+        # freq = Counter(words[0])
+        # for word in words:
+        #     freq &= Counter(word)
+        # return list(freq.elements())
+        common = list(words[0])
+        for word in words[1:]:
+            new_common = []
+            for ch in word:
+                if ch in common:
+                    new_common.append(ch)
+                    common.remove(ch)
+            common = new_common
+        return common

efc547de192b213ba4351d84219e1691630f4cae
Kiran-Raj-R <56603899+Kiran-Raj-R@users.noreply.github.com>
2025-10-08 09:52:37 +0530

    Time: 47 ms (7.53%), Space: 17.3 MB (99.87%) - LeetHub

--- 1678-goal-parser-interpretation/1678-goal-parser-interpretation.py
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.py
@@ -1,14 +1,25 @@
 class Solution:
     def interpret(self, command: str) -> str:
         res = ""
-        b = ""
-        for i in range(len(command)):
-            if command[i] == '(' and command[i + 1] == ')':
+        i = 0
+        # b = ""
+        # for i in range(len(command)):
+        #     if command[i] == '(' and command[i + 1] == ')':
+        #         res += 'o'
+        #     elif command[i] == '(':
+        #         b += '(' 
+        #     elif command[i] == ')':
+        #         b += ')'
+        #     else:
+        #         res += command[i]
+        while i < len(command):
+            if command[i] == 'G':
+                res += 'G'
+                i += 1
+            elif command[i:i+2] == '()':
                 res += 'o'
-            elif command[i] == '(':
-                b += '(' 
-            elif command[i] == ')':
-                b += ')'
-            else:
-                res += command[i]
+                i += 2
+            elif command[i:i+4] == '(al)':
+                res += 'al'
+                i += 4 
         return res

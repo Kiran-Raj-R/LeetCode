06d82162aa9c9d65eb409963083cdebf7340fee9
Kiran-Raj-R <56603899+Kiran-Raj-R@users.noreply.github.com>
2025-10-22 12:32:40 +0530

    Time: 0 ms (100%), Space: 17.9 MB (8.04%) - LeetHub

---
+++ 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.py
@@ -0,0 +1,11 @@
+class Solution:
+    def balancedStringSplit(self, s: str) -> int:
+        count,balance = 0,0
+        for char in s:
+            if char == 'L':
+                balance += 1
+            else:
+                balance -= 1           
+            if balance == 0:
+                count += 1
+        return count

44e187093bdbac606b6b9cde7d542f7cd821131c
Kiran-Raj-R <56603899+Kiran-Raj-R@users.noreply.github.com>
2025-10-23 10:46:28 +0530

    Time: 31 ms (74.89%), Space: 17.8 MB (71.17%) - LeetHub

--- 3461-check-if-digits-are-equal-in-string-after-operations-i/3461-check-if-digits-are-equal-in-string-after-operations-i.py
+++ 3461-check-if-digits-are-equal-in-string-after-operations-i/3461-check-if-digits-are-equal-in-string-after-operations-i.py
@@ -3,7 +3,7 @@ class Solution:
         d = [int(digit) for digit in s]
         while len(d) > 2:
             new = []
-            for i in range(0,len(d) - 1):
+            for i in range(len(d) - 1):
                 psum = d[i] + (d[i+1] if i+1 < len(s) else 0)
                 new.append(psum % 10)
             d = new

44548b5d76780074748a48afce197597c3035b5b
Kiran-Raj-R <56603899+Kiran-Raj-R@users.noreply.github.com>
2025-10-25 11:12:37 +0530

    Time: 3 ms (39.58%), Space: 17.6 MB (98.65%) - LeetHub

--- 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.py
+++ 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.py
@@ -1,4 +1,15 @@
 class Solution:
     def totalMoney(self, n: int) -> int:
-        q, r = divmod(n,7)
-        return 28*q+7*q*(q-1)//2 + (2*q+r+1)*r//2
+        # q, r = divmod(n,7)
+        # return 28*q+7*q*(q-1)//2 + (2*q+r+1)*r//2
+        mon = 0
+        c , res = 0,0
+        for i in range(1,n+1):
+            if c == 7:
+                mon = i // 7 + 1
+                c = 0
+            else:
+                mon += 1
+            res += mon
+            c += 1
+        return res

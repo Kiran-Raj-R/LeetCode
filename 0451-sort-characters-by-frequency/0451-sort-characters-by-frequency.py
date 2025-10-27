d16399e0ccab2c850a24edffc378b39742286ed5
Kiran-Raj-R <56603899+Kiran-Raj-R@users.noreply.github.com>
2025-10-27 11:12:57 +0530

    Time: 13 ms (32.47%), Space: 18.8 MB (94.94%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
@@ -7,7 +7,7 @@ class Solution:
                 freq[c] += 1
             else:
                 freq[c] = 1
-        freq2 = dict(sorted(freq.items(),key=lambda x:x[1],reverse=True))
-        for k,v in freq2.items():
-            res = res + (k*v)
+        freq2 = sorted(freq.items(),key=lambda x:x[1],reverse=True)
+        for k,v in freq2:
+            res += (k*v)
         return res

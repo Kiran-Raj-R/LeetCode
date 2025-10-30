d6462f110414d251ca54085a9fe1d34dfc46f0d3
Kiran-Raj-R <56603899+Kiran-Raj-R@users.noreply.github.com>
2025-10-30 10:03:30 +0530

    Time: 3 ms (95.87%), Space: 18 MB (35.75%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -1,7 +1,7 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
-            return False
+        # if len(s) != len(t):
+        #     return False
         hash1, hash2 = {}, {}
         for i,j in zip(s,t):
             if i in hash1:

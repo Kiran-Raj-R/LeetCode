7da590c51f4027c6b55cfda98f5caddfd050c189
Kiran-Raj-R <56603899+Kiran-Raj-R@users.noreply.github.com>
2025-11-01 10:02:49 +0530

    Time: 119 ms (86.49%), Space: 44.6 MB (96.52%) - LeetHub

---
+++ 2442-count-number-of-distinct-integers-after-reverse-operations/2442-count-number-of-distinct-integers-after-reverse-operations.py
@@ -0,0 +1,6 @@
+class Solution:
+    def countDistinctIntegers(self, nums: List[int]) -> int:
+        res = set(nums)
+        for num in nums:
+            res.add(int(str(num)[::-1]))
+        return len(res)

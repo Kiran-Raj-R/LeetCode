9a43ce1d911325e27b133b2e76e6c84251425921
Kiran-Raj-R <56603899+Kiran-Raj-R@users.noreply.github.com>
2025-11-07 10:43:47 +0530

    Time: 0 ms (100%), Space: 17.7 MB (49.17%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -1,6 +1,7 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        res = 0
-        for digit in digits:
-            res = (res * 10) + digit
-        return [int(d) for d in str(res+1)]
+        # res = 0
+        # for digit in digits:
+        #     res = (res * 10) + digit
+        # return [int(d) for d in str(res+1)]
+        return [int(d) for d in str(int(''.join(map(str,digits)))+1)]

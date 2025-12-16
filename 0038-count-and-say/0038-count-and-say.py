40080332525880e2defb4f7bf515f5fcd37f10fe
Kiran-Raj-R <56603899+Kiran-Raj-R@users.noreply.github.com>
2025-12-16 11:48:16 +0530

    Time: 3 ms (98.51%), Space: 18.2 MB (9.24%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.py
@@ -0,0 +1,17 @@
+class Solution:
+    def countAndSay(self, n: int) -> str:
+        res = '1'
+        for _ in range(1,n):
+            seq = []
+            seq_append = seq.append
+            prev = res[0]
+            cnt = 1
+            for ch in res[1:]:
+                if ch == prev:
+                    cnt += 1
+                else:
+                    seq_append(str(cnt));seq_append(prev)
+                    prev = ch; cnt = 1
+            seq_append(str(cnt)); seq_append(prev)
+            res = ''.join(seq)
+        return res

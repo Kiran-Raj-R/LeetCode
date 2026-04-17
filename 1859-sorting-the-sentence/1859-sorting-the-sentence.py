765f258821344fd6e2d96e79d7a69053ce5d557f
Kiran-Raj-R <56603899+Kiran-Raj-R@users.noreply.github.com>
2026-04-17 08:11:02 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1859-sorting-the-sentence/1859-sorting-the-sentence.py
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.py
@@ -1,18 +1,8 @@
 class Solution:
     def sortSentence(self, s: str) -> str:
-        # words = s.split()
-        # result = [""] * len(words)
-
-        # for word in words:
-        #     position = int(word[-1]) - 1
-        #     result[position] = word[:-1]
-
-        # return " ".join(result)   
-        words = [''] * 9
-        sParsed = s.split()          
-
-        for word in sParsed:
-            j = ord(word[-1]) - 49
-            words[j] = word[:-1]
-
-        return " ".join(words).rstrip() 
+        words = s.split()
+        result = [""] * len(words)
+        for word in words:
+            position = int(word[-1]) - 1
+            result[position] = word[:-1]
+        return " ".join(result)   

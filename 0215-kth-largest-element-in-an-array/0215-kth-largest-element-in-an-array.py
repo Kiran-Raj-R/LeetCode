16ba1deb21d2cbfd03026d676f9c1b454c8af1b7
Kiran-Raj-R <56603899+Kiran-Raj-R@users.noreply.github.com>
2026-07-22 09:15:21 +0530

    Time: 1263 ms (5.02%), Space: 31.3 MB (19.87%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -1,10 +1,24 @@
-import heapq
 class Solution:
+    def heapify(self, arr, n, i):
+        largest = i
+        left = 2*i + 1
+        right = 2*i + 2
+        if left < n and arr[left] > arr[largest]:
+            largest = left
+        if right < n and arr[right] > arr[largest]:
+            largest = right
+        if largest != i:
+            arr[i], arr[largest] = arr[largest], arr[i]
+            self.heapify(arr, n, largest)
+
+    def heapSort(self, arr):
+        n = len(arr)
+        for i in range(n//2-1, -1, -1):
+            self.heapify(arr, n, i)
+        for i in range(n-1, 0, -1):
+            arr[0], arr[i] = arr[i], arr[0]
+            self.heapify(arr, i, 0)
+
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        heap = []
-        for num in nums:
-            if len(heap) < k:
-                heapq.heappush(heap, num)
-            elif num > heap[0]:
-                heapq.heapreplace(heap, num)
-        return heap[0]
+        self.heapSort(nums)
+        return nums[-k]
